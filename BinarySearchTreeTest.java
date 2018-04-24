e12ba9fe712faf9a4525dc739cc0067c63d5f4df
yotsaphonSutweha <komsan_58@hotmail.com>
2018-04-24 17:09:11 +0100

    put in test cases

--- BinarySearchTreeTest.java
+++ BinarySearchTreeTest.java
@@ -1,24 +1,53 @@
+import org.junit.Before;
+
 import static org.junit.Assert.*;
 
 public class BinarySearchTreeTest {
 
+    private BinarySearchTree bst;
+    private BTNode node;
+
+    @Before
+    public void setUp() throws Exception {
+        bst = new BinarySearchTree();
+    }
+
     @org.junit.Test
-    public void theRoot() {
+    public void theRootIsNull() {
+        bst.insertNode(null, node);
+        assertEquals(null, bst.theRoot());
+    }
+
+    @org.junit.Test
+    public void theRootNotNull() {
+        node = new BTNode(1);
+        bst.insertNode(bst.theRoot(), node);
+        assertEquals(1,1);
     }
 
     @org.junit.Test
     public void isEmpty() {
+        bst.insertNode(bst.theRoot(), node);
+        assertEquals(false, false);
     }
 
     @org.junit.Test
     public void insertNode() {
+        node = new BTNode(3);
+        bst.insertNode(bst.theRoot, node);
+        assertEquals(3, 3);
     }
 
     @org.junit.Test
     public void search() {
+        node = new BTNode(3);
+        bst.insertNode(bst.theRoot, node);
+        bst.search(3, bst.theRoot());
+        assertEquals(3,3);
     }
 
     @org.junit.Test
     public void inOrderTraversal() {
+        fail();
     }
 }
